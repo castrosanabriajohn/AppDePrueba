401d1c2c6bf943ab9043313069c05476d89483d3
John <castrosanabriajohn@gmail.com>
2026-01-17 18:42:22 -0600

    Initial Commit

---
+++ AppDePrueba/AppDePruebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppDePruebaApp.swift
+//  AppDePrueba
+//
+//  Created by John Benjamín Castro Sanabria on 17/1/26.
+//
+
+import SwiftUI
+
+@main
+struct AppDePruebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppDePrueba/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppDePrueba
+//
+//  Created by John Benjamín Castro Sanabria on 17/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
